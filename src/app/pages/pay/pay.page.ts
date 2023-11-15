93c1fba899a74d52a71ecff97763919dfdd39f20
Berdrago <x56dragon@gmail.com>
2023-11-14 23:59:25 -0300

    cambios 14 pago Boleta pdf

--- src/app/pages/pay/pay.page.ts
+++ src/app/pages/pay/pay.page.ts
@@ -6,6 +6,13 @@ import { ToastService } from '../../services/toast.service';
 import { AlertController, LoadingController } from '@ionic/angular';
 import { OrderService } from '../../services/order.service';
 import { Order } from '../../models/interfaceOrder';
+import { SharedService } from '../../services/shared.service';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { UserService } from 'src/app/services/user.service';
+
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-pay',
@@ -17,7 +24,9 @@ export class PayPage implements OnInit {
   saleId: string | null = null;
   orders: Order[] = [];
   propinaIngresada: number | null = null;
-
+  sharedDataService: SharedService;
+  user: any;
+  userEmail!: string;
   constructor(
     private toastService: ToastService,
     private auth: AuthService,
@@ -26,7 +35,10 @@ export class PayPage implements OnInit {
     private loadingController: LoadingController,
     private alertController: AlertController,
     private orderService: OrderService,
+    sharedDataService: SharedService,
+    private userService: UserService,
   ) {
+    this.sharedDataService = sharedDataService;
     this.saleId = localStorage.getItem('saleId');
   }
 
@@ -34,6 +46,9 @@ export class PayPage implements OnInit {
     this.token = this.auth.getToken();
     this.token = this.auth.getToken();
     this.order();
+    this.user = this.userService.getUser();
+    this.userEmail = this.user.mail;
+    console.log('Correo electrónico del usuario:', this.userEmail);
   }
 
   getTotal(): number {
@@ -116,7 +131,9 @@ export class PayPage implements OnInit {
             }
             this.orderService.pay(saleId, this.token).subscribe({
               next: (response) => {
+                this.generatePDF();
                 localStorage.removeItem('saleId');
+                this.sharedDataService.setMostrarCarta(false);
                 this.router.navigate(['clientes', this.token, 'tab1']);
                 this.toastService.showToast('Se ha realizado el pago de manera exitosa', 'success', 3000);
                 this.localNotificationsService.scheduleNotification(
@@ -135,5 +152,51 @@ export class PayPage implements OnInit {
       ]
     });
     await alert.present();
+  }
+
+
+  generatePDF(): void {
+    const documentDefinition: TDocumentDefinitions = {
+      content: [
+        { text: 'Boleta de Compra', style: 'header' },
+        { text: 'Detalles de la orden:', style: 'subheader' },
+        this.generateProductList(),
+        { text: `Total a pagar: $${this.getTotalAPagar()} pesos`, style: 'total' },
+        { text: `Iva: $${this.getIva()} pesos`, style: 'details' },
+        { text: `Propina: $${this.getPropina()} pesos`, style: 'details' },
+        { text: '¡Gracias por su compra!', style: 'footer' }
+      ],
+      styles: {
+        header: { fontSize: 22, bold: true, alignment: 'center', margin: [0, 0, 0, 15] },
+        subheader: { fontSize: 18, bold: true, margin: [0, 15, 0, 5] },
+        total: { fontSize: 16, bold: true, margin: [0, 15, 0, 15] },
+        details: { fontSize: 14, margin: [0, 5, 0, 5] },
+        footer: { fontSize: 14, bold: true, alignment: 'center', margin: [0, 15, 0, 0] },
+      },
+    };
+    const pdfDocGenerator = pdfMake.createPdf(documentDefinition);
+    pdfDocGenerator.download('boleta_compra.pdf');
+
+    // const pdfDocGenerator = pdfMake.createPdf(documentDefinition);
+    // pdfDocGenerator.download('boleta_compra.pdf');
+    // const pdfDocGenerator = pdfMake.createPdf(documentDefinition);
+    // pdfDocGenerator.getBlob((blob) => {
+    //   const filename = 'boleta_compra.pdf';
+    //   const objectURL = URL.createObjectURL(blob);
+
+    //   const mailtoLink = `mailto:${userEmail}?subject=Boleta%20de%20Compra&body=Adjunto%20encontrarás%20la%20boleta%20de%20compra%20en%20formato%20PDF.%0D%0A%0D%0A¡Gracias!&attachment=${objectURL}`;
+
+    //   window.location.href = mailtoLink;
+    // });
+  }
+  generateProductList(): any[] {
+    const productList = this.orders.map(order => {
+      return [
+        { text: order.food.name, style: 'product' },
+        { text: `$${order.food.price} pesos`, style: 'price' },
+      ];
+    });
+
+    return productList;
   }
 }
